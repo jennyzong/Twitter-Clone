0b9be7eea1f3b52be0e869e71d1c533d3197ada3
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-06-02 22:05:05 -0400

    Add textArea
    -Add a textArea.
    -Have the eventListener's function log out the contents of the textArea when the Tweet button is clicked.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-/*
-Challenge:
-1. Put all of the data in its own file called
-   data.js, and export it back into index.js. 
-   Make any changes to index.html that are
-   necessary to make this work.
-2. Log out tweetsData.
-*/
+import { tweetsData } from './data.js'
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
 
-import {tweetsData} from '/data.js'
-console.log(tweetsData)
+tweetBtn.addEventListener('click', function(){
+    console.log(tweetInput.value)
+/*
+Challenge
+1. Have the eventListener's function
+   log out the contents of the textarea
+   when the Tweet button is clicked.
+*/
+})
